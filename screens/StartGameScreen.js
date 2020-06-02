26cda85670e84c53ba1324a6af725eda30d5d305
naman_yadav <namanyadav@gmail.com>
2020-06-02 17:24:16 -0400

    adding colors constants

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 import Card from "../components/Card";
+import colors from "../constants/colors";
 
 const StartGameScreen = props => {
     return (
@@ -10,8 +11,8 @@ const StartGameScreen = props => {
                 <Text>Select a Number!</Text>
                 <TextInput/>
                 <View style={styles.buttonContainer}>
-                    <Button title="Reset" onPress={() => {}}/>
-                    <Button title="Confirm" onPress={() => {}}/>
+                    <View style={styles.button}><Button title="Reset" onPress={() => {}} color={colors.accent}/></View>
+                    <View style={styles.button}><Button title="Confirm" onPress={() => {}} color={colors.primary}/></View>
                 </View>
             </Card>
         </View>
@@ -37,6 +38,9 @@ const styles = StyleSheet.create({
         width: '100%',
         justifyContent: 'space-between',
         paddingHorizontal: 15
+    },
+    button: {
+        width: 100
     }
 });
 export default StartGameScreen;
