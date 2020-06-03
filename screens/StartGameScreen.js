46ce50c426972c3235079b3a373f4cce9ca1a08d
naman_yadav <namanyadav@gmail.com>
2020-06-03 12:18:05 -0400

    adding a MainButton

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -6,6 +6,7 @@ import Input from "../components/Input";
 import NumberContainer from "../components/NumberContainer";
 import BodyText from "../components/BodyText";
 import TitleText from "../components/TitleText";
+import MainButton from "../components/MainButton";
 
 const StartGameScreen = props => {
     const [enteredValue, setEnteredValue] = useState('')
@@ -39,7 +40,7 @@ const StartGameScreen = props => {
                 <NumberContainer>
                     {selectedNumber}
                 </NumberContainer>
-                <Button title="START GAME" onPress={() => props.onStartGame(selectedNumber)}></Button>
+                <MainButton onPress={() => props.onStartGame(selectedNumber)}>START GAME</MainButton>
             </Card>
         )
     }
