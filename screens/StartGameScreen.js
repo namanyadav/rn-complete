2d137807702df4a41366e91d49b1e5fcb82320e2
naman_yadav <namanyadav@gmail.com>
2020-06-03 18:54:41 -0400

    using Dimensions API

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, Button, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button, TouchableWithoutFeedback, Keyboard, Alert, Dimensions } from 'react-native';
 import Card from "../components/Card";
 import colors from "../constants/colors";
 import Input from "../components/Input";
@@ -84,8 +84,9 @@ const styles = StyleSheet.create({
         fontFamily: 'open-sans-bold'
     },
     inputContainer: {
-        width: 300,
-        maxWidth: '80%',
+        width: '80%',
+        minWidth: 300,
+        maxWidth: '95%',
         alignItems: 'center'
     },
     buttonContainer: {
@@ -95,7 +96,8 @@ const styles = StyleSheet.create({
         paddingHorizontal: 15
     },
     button: {
-        width: 100
+        // width: 100
+        width: Dimensions.get('window').width / 4
     },
     input : {
         width: 50,
