a7443541aeca11d8944af3214ce0413348e323d1
naman_yadav <namanyadav@gmail.com>
2020-06-03 19:15:37 -0400

    fixing game over screen. scroll view and dynamic sizing/loyout

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button, Image } from 'react-native';
+import { View, Text, StyleSheet, Button, Image, Dimensions, ScrollView } from 'react-native';
 import BodyText from "../components/BodyText";
 import TitleText from "../components/TitleText";
 import colors from "../constants/colors";
@@ -7,24 +7,27 @@ import MainButton from "../components/MainButton";
 
 const GameOverScreen = props => {
     return (
-        <View style={styles.screen}>
-            <TitleText>The Game is Over!</TitleText>
-            <View style={styles.imageContainer}>
-                <Image
-                    style={styles.image}
-                    // source={{uri: 'https://cdn.mos.cms.futurecdn.net/ntFmJUZ8tw3ULD3tkBaAtf-970-80.jpg'}}
-                    source={require('../assets/success.png')}
-                    resizeMode='cover' />
-            </View>
+        <ScrollView>
+            <View style={styles.screen}>
+                <TitleText>The Game is Over!</TitleText>
+                <View style={styles.imageContainer}>
+                    <Image
+                        style={styles.image}
+                        // source={{uri: 'https://cdn.mos.cms.futurecdn.net/ntFmJUZ8tw3ULD3tkBaAtf-970-80.jpg'}}
+                        source={require('../assets/success.png')}
+                        resizeMode='cover' />
+                </View>
 
-            <View style={styles.resultContainer}>
-                <BodyText style={styles.resultText}>Your mobile took
-                    {' '} <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to guess the number
-                    {' '} <Text style={styles.highlight}>{props.userNumber}</Text>
-                </BodyText>
+                <View style={styles.resultContainer}>
+                    <BodyText style={styles.resultText}>Your mobile took
+                        {' '} <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to guess the number
+                        {' '} <Text style={styles.highlight}>{props.userNumber}</Text>
+                    </BodyText>
+                </View>
+                <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
             </View>
-            <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
-        </View>
+        </ScrollView>
+
     )
 };
 
@@ -39,13 +42,13 @@ const styles = StyleSheet.create({
         height: '100%'
     },
     imageContainer: {
-        width: 300,
-        height: 300,
-        borderRadius: 150,
+        width: Dimensions.get('window').width * 0.7,
+        height: Dimensions.get('window').width * 0.7,
+        borderRadius: Dimensions.get('window').width * 0.7 / 2,
         borderWidth: 3,
         borderColor: 'black',
         overflow: 'hidden',
-        marginVertical: 30
+        marginVertical: Dimensions.get('window').height / 30
     },
     highlight: {
         color: colors.primary,
@@ -53,11 +56,12 @@ const styles = StyleSheet.create({
     },
     resultContainer: {
         marginHorizontal: 40,
-        marginVertical: 15
+        marginVertical: Dimensions.get('window').height / 60
     },
     resultText: {
         textAlign: 'center',
-        fontSize: 20
+        // fontSize: 20,
+        fontSize: Dimensions.get('window').height < 100 ? 16 : 20
     }
 });
 
