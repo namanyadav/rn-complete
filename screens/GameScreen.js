23b03be3ee51b38fb3ce149731d30c1ced446bc7
naman_yadav <namanyadav@gmail.com>
2020-06-03 19:01:56 -0400

    dynamic css dimensions api

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { View, Text, StyleSheet, Button, Alert, ScrollView, FlatList } from 'react-native';
+import { View, Text, StyleSheet, Button, Alert, ScrollView, FlatList, Dimensions } from 'react-native';
 import NumberContainer from "../components/NumberContainer";
 import Card from "../components/Card";
 import DefaultStyles from "../constants/default-styles";
@@ -98,7 +98,7 @@ const styles = StyleSheet.create({
     buttonContainer: {
         flexDirection: 'row',
         justifyContent: 'space-around',
-        marginTop: 20,
+        marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
         width: 300,
         maxWidth: '80%'
     },
@@ -118,7 +118,7 @@ const styles = StyleSheet.create({
         flexGrow: 1
     },
     listContainer: {
-        width: '60%',
+        width: Dimensions.get('window').width > 350 ? '60%' : '80%',
         flex: 1
     }
 });
